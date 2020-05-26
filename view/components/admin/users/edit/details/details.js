f949098c5c32c55b85ee119b93690d85fdf524af
Arikaim <sales@intersoft.biz>
2020-05-26 11:12:06 +0300

    =

--- view/components/admin/users/edit/details/details.js
+++ view/components/admin/users/edit/details/details.js
@@ -1,6 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
+    arikaim.ui.form.addRules("#create_user_form",{
+        inline: false,
+        fields: {
+            user_name: {
+                rules: [{ type: 'minLength[2]' }]
+            },
+            email: {
+                rules: [{ type: 'email' }]
+            },
+            password: {
+                rules: [{
+                    type: "minLength[4]"
+                }]
+            },
+            repeat_password: {
+                rules: [
+                    { type: "minLength[4]" },
+                    { type: "match[password]" }
+                ]
+            }
+        }
+    });  
     arikaim.ui.form.onSubmit('#create_user_form',function() {
        return usersAdmin.update('#create_user_form');
     },function(result) {       
--- view/components/admin/users/form/form.js
+++ view/components/admin/users/form/form.js
@@ -2,26 +2,4 @@
 
 $(document).ready(function() { 
     arikaim.ui.viewPasswordButton('.view-password');
-    arikaim.ui.form.addRules("#create_user_form",{
-        inline: false,
-        fields: {
-            user_name: {
-                rules: [{ type: 'minLength[2]' }]
-            },
-            email: {
-                rules: [{ type: 'email' }]
-            },
-            password: {
-                rules: [{
-                    type: "minLength[4]"
-                }]
-            },
-            repeat_password: {
-                rules: [
-                    { type: "minLength[4]" },
-                    { type: "match[password]" }
-                ]
-            }
-        }
-    });  
 });
