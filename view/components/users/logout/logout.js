cda921333c7d9a562803b2692fe536cd5bba287f
arikaim <info@arikaim.com>
2022-04-27 06:49:04 -0400

    =

--- view/components/users/logout/logout.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-arikaim.component.onLoaded(function() {
-    users.logout(function(result) {
-        var redirectUrl = (isEmpty(result.redirect_url) == true) ? '/' : result.redirect_url;
-        arikaim.loadUrl(redirectUrl);
-    });
-});
