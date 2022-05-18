b7ad5eab24d709b9c9d4a5aedd143a68007a7879
Arikaim <info@arikaim.com>
2022-05-18 22:53:33 +0300

    =

--- subscribers/OauthSubscriber.php
+++ subscribers/OauthSubscriber.php
@@ -39,23 +39,27 @@ class OauthSubscriber extends EventSubscriber implements EventSubscriberInterfac
         $data = $event->getParameters(); 
         $action = $data['action'] ?? null;
         $user = $data['user'];
+        $userName = $user['user_name'] ?? '';
+        $email = $user['email'] ?? '';
 
         $users = Model::Users();
         $tokens = Model::OauthTokens('oauth');
        
-        // Find user
-        $userFound = $users->getUser($user['user_name'],$user['email']); 
-        if ($userFound == false) {
-            // create user
-            $password = Utils::createRandomKey();
-            $newUser = $users->createUser($user['user_name'],$password,$user['email']);
-            $userId = $newUser->id;            
-        } else {
-            $userId = $userFound->id;
+        // User signup
+        if (empty($userName) == false || empty($email) == false) {
+            $userFound = $users->getUser($userName,$email); 
+            if ($userFound == false) {
+                // create user
+                $password = Utils::createRandomKey();
+                $newUser = $users->createUser($userName,$password,$email);
+                $userId = $newUser->id;            
+            } else {
+                $userId = $userFound->id;
+            }
+
+            $tokens->saveUserId($data['access_token'],$data['driver'],$userId);   
         }
        
-        $tokens->saveUserId($data['access_token'],$data['driver'],$userId);   
-
         if ($action == 'login') {
             // login with oauth provider
             Arikaim::access()->withProvider('oauth',$tokens)->authenticate([
